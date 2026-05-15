de049ea8ef53efcd9b54de147d3a483cd956b78f
Chris82111 <75613723+Chris82111@users.noreply.github.com>
2026-05-15 15:06:03 +0200

    Added empty IDE directories for C and C++

---
+++ test/c/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int errors = -1;
+
+  printf("Number of errors is %i\n", errors);
+}
+
---
+++ test/cpp/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+  int errors = -1;
+
+  cout << "The number of errors is " << errors << endl;
+} 
